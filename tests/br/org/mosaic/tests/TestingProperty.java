4b81e22b482d57f8bb29f462ec66f70b310d29e7
Andrew S Aguiar <andrewaguiar6@gmail.com>
2012-03-03 19:36:19 -0300

    test jenkins

--- tests/br/org/mosaic/tests/TestingProperty.java
+++ tests/br/org/mosaic/tests/TestingProperty.java
@@ -11,13 +11,13 @@ public class TestingProperty extends TestCase {
 	public void testCreatingSimpleProperty() {
 		final Property p = new Property("TESTE", "VALUE");
 
-		Assert.assertEquals("TESTE='VALUE' ", p.toString());
+		Assert.assertEquals("TESTE='VALUE'", p.toString());
 	}
 
 	public void testCreatingSimplePropertyWithEmptyValue() {
 		final Property p = new Property("TESTE", "");
 
-		Assert.assertEquals("TESTE='' ", p.toString());
+		Assert.assertEquals("TESTE=''", p.toString());
 	}
 
 	public void testShouldntAllowCreateAPropertyUnamed() {
@@ -71,19 +71,19 @@ public class TestingProperty extends TestCase {
 		final Property p = new Property("quotation", "single's");
 		p.setQuotation(Quotation.SINGLE);
 
-		Assert.assertEquals(" quotation='single\\'s'", p.toString());
+		Assert.assertEquals("quotation='single\\'s'", p.toString());
 	}
 
 	public void testDoubleQuotationWithValue() {
 		final Property p = new Property("quotation", "double\"s");
 		p.setQuotation(Quotation.DOUBLE);
 
-		Assert.assertEquals(" quotation=\"double\\\"s\"", p.toString());
+		Assert.assertEquals("quotation=\"double\\\"s\"", p.toString());
 	}
 
 	public void testCreatingEventProperty() {
 		final EventProperty p = EventProperty.onmousedown("function(){teste();}");
 
-		Assert.assertEquals(" onmousedown='function(){teste();}'", p.toString());
+		Assert.assertEquals("onmousedown='function(){teste();}'", p.toString());
 	}
 }
